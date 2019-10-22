d892fd91bba7d2546b7a4a25155a070678f3c524
Kevin Engström <kevin.engstrom@hotmail.com>
2019-10-22 09:00:55 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DataLagringFörstaInlämning.Domain;
+
+
+
+namespace DataLagringFörstaInlämning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
