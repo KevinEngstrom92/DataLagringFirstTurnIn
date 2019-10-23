ce20fcc512805db60740cda7d3091410067952dd
Kevin Engström <kevin.engstrom@hotmail.com>
2019-10-23 12:39:12 +0200

    Add files for program

--- Domain/SqlClass.cs
+++ Domain/SqlClass.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-
 namespace DataLagringFörstaInlämning.Domain
 {
-    class Registration
+    class SqlClass
     {
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,39 @@ namespace DataLagringFörstaInlämning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool shouldRun = true;
+
+            while (shouldRun) 
+            {
+
+                Console.WriteLine("1. Ny reservation\n2.Lista reservationer\n3.Utför besiktning\n4.Avsluta");
+
+                ConsoleKeyInfo input = Console.ReadKey(true);
+
+                switch (input.Key) 
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+                    case ConsoleKey.D2:
+                        break;
+                    case ConsoleKey.D3:
+
+                        break;
+                    case ConsoleKey.D4:
+                        shouldRun = false;
+                        break;
+
+
+
+                }
+
+
+
+
+
+
+            }
         }
     }
 }
