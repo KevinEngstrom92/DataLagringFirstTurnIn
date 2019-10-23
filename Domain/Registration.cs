07043db00ecb7c69d85bdf5e4d310f746f87b532
Kevin Engström <kevin.engstrom@hotmail.com>
2019-10-23 14:07:32 +0200

    Add Registration class

--- Domain/Registration.cs
+++ Domain/Registration.cs
@@ -5,7 +5,26 @@ using System.Text;
 
 namespace DataLagringFörstaInlämning.Domain
 {
-    class Registration
+    class Reservation
     {
+
+       
+            public int Id { get; }
+            public string RegistrationNumber { get; }
+            public DateTime Date { get; }
+            
+
+            public Reservation(string registrationNumber, DateTime date)
+            {
+                RegistrationNumber = registrationNumber;
+                Date = date;
+            }
+            public Reservation(int id, string registrationNumber, DateTime date)
+            {
+                Id = id;
+                RegistrationNumber = registrationNumber;
+                Date = date;
+            }
+        
     }
 }
